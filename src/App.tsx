88d2e747e44aed73bcdfc2e3a477695e00cae664
olarotimi <ademolarotimi2000@gmail.com>
2023-04-05 13:25:28 +0100

    feat: navbar

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import Navbar from "@/scenes/navbar"
+import { useEffect, useState } from "react"
+import { SelectedPage } from "./shared/types";
+
+
+
+
+
+
+function App() {
+
+  const[selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home);
+
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+
+
+  useEffect(()=>{
+    const handleScroll = () =>{
+      if(window.scrollY === 0){
+        setIsTopOfPage(true);
+        setSelectedPage(SelectedPage.Home);
+      }
+      if(window.scrollY !== 0){
+        setIsTopOfPage(false);
+      }
+      window.addEventListener("scroll", handleScroll);
+
+      return () => window.removeEventListener("scroll", handleScroll)
+    }
+  }, []);
+    
+
+  return (
+    <div className="app bg-gray-20">
+      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} isTopOfPage={isTopOfPage}/>
+     
+    </div>
+  )
+}
+
+export default App
